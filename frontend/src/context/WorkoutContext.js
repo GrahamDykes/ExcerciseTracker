9109963693f6bff32b46c6f1e30d29d0d3b66e13
GrahamDykes <153469002+GrahamDykes@users.noreply.github.com>
2024-05-19 14:03:29 -0700

    Actually Done. For real

--- frontend/src/context/WorkoutContext.js
+++ frontend/src/context/WorkoutContext.js
@@ -1,24 +1,34 @@
-import { createContext, useReducer } from "react";
+import { createContext, useReducer } from 'react'
 
-export const WorkoutsContext = createContext();
+export const WorkoutsContext = createContext()
 
-//takes in reliable old state, and action(& payload) from the dispatch function
-export const workoutsReducer = (state,action)=>{
-switch (action.type){
-    case "SET_WORKOUTS": return{workouts:action.payload}
-    case 'CREATE_WORKOUT': return{workouts:[action.payload,...state.workouts]}
-    case 'DELETE_WORKOUT': return{
-        workouts: state.workouts.filter( (e)=> e._id !== action.payload._id)
-    }
-    default: return state
+export const workoutsReducer = (state, action) => {
+  switch (action.type) {
+    case 'SET_WORKOUTS': 
+      return {
+        workouts: action.payload
+      }
+    case 'CREATE_WORKOUT':
+      return {
+        workouts: [action.payload, ...state.workouts]
+      }
+    case 'DELETE_WORKOUT':
+      return {
+        workouts: state.workouts.filter((w) => w._id !== action.payload._id)
+      }
+    default:
+      return state
+  }
 }
 
+export const WorkoutsContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(workoutsReducer, {
+    workouts: null
+  })
+
+  return (
+    <WorkoutsContext.Provider value={{...state, dispatch}}>
+      { children }
+    </WorkoutsContext.Provider>
+  )
 }
-
-export const WorkoutsContextProvider = ({children}) => {
-    const [state, dispatch] = useReducer(workoutsReducer, {workouts:null})
-
-  return <WorkoutsContext.Provider value={{...state,dispatch}}>
-    {children}
-    </WorkoutsContext.Provider>;
-};
